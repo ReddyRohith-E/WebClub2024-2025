e5fc737da9041173da5ee65ac887982a4d8dc014
Rohith Enduluri <e.reddyrohith2004@gmail.com>
2025-01-26 20:43:10 +0530

    Added navigation and footer components with styling

--- assets/JS/components.js
+++ assets/JS/components.js
@@ -1,4 +1,21 @@
 document.addEventListener("DOMContentLoaded", function() {
+    
+    // Load nav
+    fetch("assets/Components/nav.html")
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.text();
+    })
+    .then(data => {
+        document.body.insertAdjacentHTML("afterbegin", data);
+    })
+    .catch(error => {
+        console.error('There was a problem with the fetch operation for nav:', error);
+    });
+
+    // Load footer
     fetch("assets/Components/footer.html")
         .then(response => {
             if (!response.ok) {
