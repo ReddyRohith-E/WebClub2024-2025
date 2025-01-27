f0bf1756be433b7d5af51ee8635f9d2d02f5cacf
V Mahesh Kumar <maheshkumarvmk0@gmail.com>
2025-01-27 11:12:42 +0530

    Pushed

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,12 +1,13 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const projectData = document.getElementById('project-data').textContent;
-    const projects = JSON.parse(projectData);
-    const container = document.getElementById('project-cards-container');
-    
-    projects.forEach(project => {
-        const card = document.createElement('div');
-        card.className = 'project-card';
-        card.innerHTML = `
+document.addEventListener("DOMContentLoaded", () => {
+	const projectData = document.getElementById("project-data").textContent;
+	const projects = JSON.parse(projectData);
+	const container = document.getElementById("project-container");
+    console.log(container)
+
+	projects.forEach((project) => {
+		const card = document.createElement("div");
+		card.className = "project-card";
+		card.innerHTML = `
             <div class="overlay">
                 <img src="${project.image}" alt="${project.alt}">
                 <h2>${project.title}</h2>
@@ -14,6 +15,6 @@ document.addEventListener('DOMContentLoaded', () => {
             <p>${project.description}</p>
             <button class="exp-btn">Explore</button>
         `;
-        container.appendChild(card);
-    });
+		container.appendChild(card);
+	});
 });
