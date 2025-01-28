d2683d3e959d999797f73575d2c5cd9ec5d82fae
Rohith Enduluri <e.reddyrohith2004@gmail.com>
2025-01-28 11:29:53 +0530

    Add responsive hamburger menu and improve navbar styles

--- assets/JS/components.js
+++ assets/JS/components.js
@@ -10,6 +10,7 @@ document.addEventListener("DOMContentLoaded", function() {
     })
     .then(data => {
         document.body.insertAdjacentHTML("afterbegin", data);
+        document.querySelector('.hamburger').addEventListener('click', toggleMenu);
     })
     .catch(error => {
         console.error('There was a problem with the fetch operation for nav:', error);
@@ -28,3 +29,17 @@ document.addEventListener("DOMContentLoaded", function() {
         })
         .catch(error => console.error('Error loading footer:', error));
 });
+
+function toggleMenu() {
+    var navLinks = document.querySelector('.nav-links');
+    navLinks.classList.toggle('show');
+
+    var hamburgerIcon = document.querySelector('.hamburger i');
+    if (hamburgerIcon.classList.contains('fa-bars')) {
+        hamburgerIcon.classList.remove('fa-bars');
+        hamburgerIcon.classList.add('fa-times');
+    } else {
+        hamburgerIcon.classList.remove('fa-times');
+        hamburgerIcon.classList.add('fa-bars');
+    }
+}
